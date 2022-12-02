83be64901a7bc066091741ed7db47cb45aa7930f
Caio Larroza de Oliveira <caiolarroza@hotmail.com>
2022-12-01 22:42:40 -0300

    Finalizando header, footer e filtros

---
+++ containers/Home.tsx
@@ -0,0 +1,37 @@
+import type { NextPage } from "next";
+import { useState } from "react";
+import { Filter } from '../components/Filter';
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+
+
+type HomeProps = {
+  setToken(s: string): void
+}
+
+export const Home: NextPage<HomeProps> = ({ setToken }) => {
+
+  const [previsionDateStart, setPrevisionDateStart] = useState('');
+  const [previsionDateEnd, setPrevisionDateEnd] = useState('');
+  const [status, setStatus] = useState(0);
+
+  const sair = () => {
+    localStorage.clear();
+    setToken('');
+  }
+
+  return (
+    <>
+      <Header sair={sair} />
+      <Filter
+        previsionDateStart={previsionDateStart}
+        previsionDateEnd={previsionDateEnd}
+        status={status}
+        setPrevisionDateStart={setPrevisionDateStart}
+        setPrevisionDateEnd={setPrevisionDateEnd}
+        setStatus={setStatus} />
+      <Footer />
+    </>
+  );
+
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,17 @@
+import { useEffect, useState } from 'react';
+import { Home } from '../containers/Home';
 import { Login } from '../containers/Login';
 
-export default function Home() {
-  return (
-    <Login />
-  )
+export default function Index() {
+
+  const [token, setToken] = useState<string | null>('');
+
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      const at = localStorage.getItem('accessToken');
+      setToken(at);
+    }
+  }, []);
+
+  return token ? <Home setToken={setToken} /> : <Login setToken={setToken} />;
 }
